113e1c1b1d28412cb6edcc25b5b4fda5d52175f8
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-09-08 16:46:39 +0900

    feat: pelican 설정 수정

--- publishconf.py
+++ publishconf.py
@@ -25,5 +25,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # SEO
 SEO_REPORT = True  # SEO report is enabled by default
 SEO_ENHANCER = True  # SEO enhancer is disabled by default
-SEO_ENHANCER_OPEN_GRAPH = True # Subfeature of SEO enhancer
-SEO_ENHANCER_TWITTER_CARDS = False # Subfeature of SEO enhancer
+SEO_ENHANCER_OPEN_GRAPH = True  # Subfeature of SEO enhancer
+SEO_ENHANCER_TWITTER_CARDS = False  # Subfeature of SEO enhancer
